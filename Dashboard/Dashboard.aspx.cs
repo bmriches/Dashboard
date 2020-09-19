3718b2e9c46fbb8b3a7d8a0f1e0b1a72e2a3cd5e
bmriches <bradley.riches@gmail.com>
2020-09-18 23:05:43 -0600

    Add files via upload

---
+++ Dashboard/Dashboard.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Web;
+using System.IO;
+
+
+namespace Dashboard.Webpages
+{
+    public partial class Dashboard : System.Web.UI.Page
+    {
+        private DBConnector conn;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["username"] == null)
+            { 
+                Response.Redirect("Login.aspx");
+            }
+            else
+            { 
+                lblUserDetails.Text = Session["username"].ToString();
+            }
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            HttpPostedFile postedFile = FileUpload1.PostedFile;
+            string fileName = Path.GetFileName(postedFile.FileName);
+            string fileExtension = Path.GetExtension(fileName);
+            int fileSize = postedFile.ContentLength;
+
+            if (fileExtension.ToLower() == ".png" || fileExtension.ToLower() == ".jpg" || fileExtension.ToLower() == ".gif" || fileExtension.ToLower() == ".nef" || fileExtension.ToLower() == ".xlsx")
+            {
+                Stream stream = postedFile.InputStream;
+                BinaryReader binaryReader = new BinaryReader(stream);
+                byte[] bytes = binaryReader.ReadBytes((int)stream.Length);
+
+                //OPEN SQL CONNECTION
+                conn = new DBConnector();
+
+                //STORE SESSION TO WRITE TO DB
+                string username = (string)Session["username"];
+                {
+                    conn.SqlQuery("EXEC LoginDB.dbo.spUploadImage @Username = @username, @FileName = @filename, @SizeBytes = @size, @ImageData = @imagedata, @CustomFileName = @customfilename, @Ext = @ext");
+                    conn.Cmd.Parameters.AddWithValue("@filename", postedFile.FileName.ToString());
+                    conn.Cmd.Parameters.AddWithValue("@customfilename", tbCustomFileName.Text);
+                    conn.Cmd.Parameters.AddWithValue("@username", username.ToString());
+                    conn.Cmd.Parameters.AddWithValue("@size", postedFile.ContentLength.ToString());
+                    conn.Cmd.Parameters.Add("@imagedata", System.Data.SqlDbType.VarBinary, -1).Value = bytes;
+                    conn.Cmd.Parameters.AddWithValue("@ext", fileExtension.ToString());
+                    conn.NonQueryEx();
+
+
+                    lblMessage.Visible = true;
+                    lblMessage.Text = "Upload Successful";
+                    lblMessage.ForeColor = System.Drawing.Color.Green;
+                    ListBox1.DataBind();
+
+
+                }
+            }
+            else
+            {
+                lblMessage.Visible = true;
+                lblMessage.Text = "Non supported file type";
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("Login.aspx");
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ Dashboard/Login.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Dashboard.Webpages
+{
+    public partial class LoginPage : System.Web.UI.Page
+    {
+        //DB CONNECTOR REFERENCE
+        private DBConnector con;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //HIDE MESSAGE LABLE
+            lblErrorMessage.Visible = false;
+        }
+
+
+        protected void btnSignUp_Click(object sender, EventArgs e)
+        {
+            //OPEN POP-UP REGISTRATION PAGE
+            string url = "SignUp.aspx";
+            string s = "window.open('" + url + "', 'popup_window', 'width=400,height=200,left=100,top=100,resizable=yes');";
+            ClientScript.RegisterStartupScript(this.GetType(), "script", s, true);
+            Session["username"] = txtUserName.Text.Trim();
+        }
+
+        
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            //CONNECT TO DB
+            con = new DBConnector();
+            con.SqlQuery("EXEC LoginDB.dbo.UserLogin_Get @Username = @usrnme, @Password = @pwrd");
+            con.Cmd.Parameters.AddWithValue("@usrnme", txtUserName.Text.Trim());
+            con.Cmd.Parameters.AddWithValue("@pwrd", txtPassword.Text.Trim());
+            con.QueryEx();
+
+            //CHECK IF USER EXISTS
+            int count = Convert.ToInt32(con.Cmd.ExecuteScalar());
+            if (count == 1)
+            {
+                Session["username"] = txtUserName.Text.Trim();
+                Response.Redirect("Dashboard.aspx");
+            }
+            else
+            {
+                lblErrorMessage.Text = "Login Failed";
+                lblErrorMessage.Visible = true;
+            };
+        }
+    }
+}
